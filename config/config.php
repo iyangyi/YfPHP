0e3e4a3c3db19d9a8ab57ba5e2a37231660f26bb
iyangyi <yangyiphper@sina.cn>
2014-07-30 21:06:30 +0800

    add user_smarty config
    add user_smarty config

--- config/config.php
+++ config/config.php
@@ -1,7 +1,15 @@
 <?php
+/**
+ * Yf框架
+ * 框架的配置文件
+ *
+ * @package config.php
+ * @author tony.yang <yangyiphper@sina.cn>
+ * @version $Id: Module.class.php 2014-07-16 $
+ */
 
 $config = array(
-
+	'user_smarty'=>true,
 	'auto_create_dir' => true,
 	'default_index_controller' => 'Index',
 	'default_index_action' => 'index',
